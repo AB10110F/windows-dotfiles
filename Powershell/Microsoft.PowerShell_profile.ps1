305c284dad26b6184a2223bc14aee398c9d2948c
A. Bryant <W.Sword.09@hotmail.com>
2023-03-12 17:07:51 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -7,5 +7,8 @@ else
 {
     oh-my-posh init pwsh --config 'C:\Users\WSwor\AppData\Local\Programs\oh-my-posh\themes\tokyo.omp.json' | Invoke-Expression
     Import-Module -Name Terminal-Icons
+    Import-Module PSReadLine
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionVIewStyle ListView
     winfetch
 }
