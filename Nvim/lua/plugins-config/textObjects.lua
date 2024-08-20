40652256ea1e0fde3cfc453f0981c235ea544344
A. Bryant <W.Sword.09@hotmail.com>
2024-08-20 17:26:22 -0600

    nvim/add textObjects plugin

---
+++ Nvim/lua/plugins-config/textObjects.lua
@@ -0,0 +1,26 @@
+require 'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["ai"] = "@conditional.outer",
+        ["ii"] = "@conditional.inner",
+      },
+
+      include_surrounding_whitespace = false,
+    },
+  },
+}
