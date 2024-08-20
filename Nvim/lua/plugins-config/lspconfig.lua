de08863dab8fdbf8efdba8fac501c1d75a068758
A. Bryant <W.Sword.09@hotmail.com>
2024-08-20 17:32:10 -0600

    nvim/modify wilder
    palette theme was starting to annoy me

--- Nvim/lua/plugins-config/lspconfig.lua
+++ Nvim/lua/plugins-config/lspconfig.lua
@@ -1,6 +1,7 @@
 require("mason").setup({
   ui = {
     border = "rounded",
+    height = 0.8,
   }
 })
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
