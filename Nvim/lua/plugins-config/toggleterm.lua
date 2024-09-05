6afe90c5a30255c632971358c44e7a38b09f4c0c
A. Bryant <W.Sword.09@hotmail.com>
2024-09-05 16:20:09 -0600

    nvim/add-custom-terms

--- Nvim/lua/plugins-config/toggleterm.lua
+++ Nvim/lua/plugins-config/toggleterm.lua
@@ -32,12 +32,26 @@ toggleterm.setup({
   },
 })
 
--- lazygit
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
 
-function _lazygit_toggle()
-  lazygit:toggle()
+local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+local ghDash   = Terminal:new({ cmd = "gh dash", hidden = true })
+local atac     = Terminal:new({ cmd = "atac", hidden = true })
+local gobang   = Terminal:new({ cmd = "gobang", hidden = true })
+
+function _cmd_toggle(n)
+  if n == 1 then
+    lazygit:toggle()
+  elseif n == 2 then
+    ghDash:toggle()
+  elseif n == 3 then
+    atac:toggle()
+  elseif n == 4 then
+    gobang:toggle()
+  end
 end
 
-vim.api.nvim_set_keymap("n", "<leader>{", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>{", "<cmd>lua _cmd_toggle(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>}", "<cmd>lua _cmd_toggle(2)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>[", "<cmd>lua _cmd_toggle(3)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>]", "<cmd>lua _cmd_toggle(4)<CR>", { noremap = true, silent = true })
