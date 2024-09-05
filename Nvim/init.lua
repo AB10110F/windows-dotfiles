00eaf903ced1390bfa6a83722c580c6ad33d387a
A. Bryant <W.Sword.09@hotmail.com>
2024-09-05 17:24:45 -0600

    nvim/remove-vscode

--- Nvim/init.lua
+++ Nvim/init.lua
@@ -1,11 +1,5 @@
-if vim.g.vscode then
-  -- VSCode extension
-  require("vscodeMaps")
-else
-  -- Neovim
-  require("settings")
-  require("plugins")
-  require("maps")
-  require("plugins-config")
-  require("autocommands")
-end
+require("settings")
+require("plugins")
+require("maps")
+require("plugins-config")
+require("autocommands")
