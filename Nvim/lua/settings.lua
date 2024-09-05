173c0a74be617c46760c228bfa5f8a0eec117381
A. Bryant <W.Sword.09@hotmail.com>
2024-09-05 17:51:24 -0600

    nvim/Replace-vim.o-with-vim.opt

--- Nvim/lua/settings.lua
+++ Nvim/lua/settings.lua
@@ -1,26 +1,26 @@
-local o = vim.o
+local o = vim.opt
 
 -- Editor Settings
 
-o.number = true                                -- show line numbers
-o.relativenumber = true                        -- use relative numbers
-o.autoindent = true                            -- copy indent from current line when starting a new line
-o.cursorline = true                            -- highlight current line
-o.expandtab = true                             -- convert tabs to spaces
-o.tabstop = 2                                  -- insert 4 spaces per tab
-o.shiftwidth = 2                               -- insert 4 spaces per indentation
-o.termguicolors = true                         -- set term GUI colors
-o.splitright = true                            -- force all vertical splits to go to the right of current window
-o.splitbelow = true                            -- force all horizontal splits to go below of the current window
-o.backup = false                               -- don't create a backup
-o.swapfile = false                             -- don't create a swap file
-o.clipboard = "unnamed"                        -- allows nvim to access to the clipboard
-o.diffopt = "vertical"                         -- open split diffs vertically
-o.scrolloff = 999                              -- keeps current line on center
-vim.opt.spelllang = { "en_us", "es_mx", "ru" } -- spellcheck for this iso-languages
-o.spell = true                                 -- Enable spellcheck
-vim.opt.spellsuggest = { "best" }
-vim.opt.fillchars = { eob = ' ' }              -- Change char ~ on blank lines
+o.number = true                          -- show line numbers
+o.relativenumber = true                  -- use relative numbers
+o.autoindent = true                      -- copy indent from current line when starting a new line
+o.cursorline = true                      -- highlight current line
+o.expandtab = true                       -- convert tabs to spaces
+o.tabstop = 2                            -- insert 4 spaces per tab
+o.shiftwidth = 2                         -- insert 4 spaces per indentation
+o.termguicolors = true                   -- set term GUI colors
+o.splitright = true                      -- force all vertical splits to go to the right of current window
+o.splitbelow = true                      -- force all horizontal splits to go below of the current window
+o.backup = false                         -- don't create a backup
+o.swapfile = false                       -- don't create a swap file
+o.clipboard = "unnamed"                  -- allows nvim to access to the clipboard
+o.diffopt = "vertical"                   -- open split diffs vertically
+o.scrolloff = 999                        -- keeps current line on center
+o.spelllang = { "en_us", "es_mx", "ru" } -- spellcheck for this iso-languages
+o.spell = true                           -- Enable spellcheck
+o.spellsuggest = { "best" }
+o.fillchars = { eob = ' ' }              -- Change char ~ on blank lines
 
 -- Enable cursor blink
 o.guicursor =
