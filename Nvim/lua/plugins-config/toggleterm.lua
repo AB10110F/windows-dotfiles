4ceffae13784d2c979756537e54b908999b3ee0d
A. Bryant <W.Sword.09@hotmail.com>
2024-12-15 13:40:04 -0600

    nvim/change pwsh for nushell

--- Nvim/lua/plugins-config/toggleterm.lua
+++ Nvim/lua/plugins-config/toggleterm.lua
@@ -1,16 +1,3 @@
-local powershell_options = {
-  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-  shellcmdflag =
-  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  shellquote = "",
-  shellxquote = "",
-}
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
-end
-
 local status, toggleterm = pcall(require, "toggleterm")
 
 if not status then
@@ -22,7 +9,7 @@ toggleterm.setup({
   insert_mappings = false,
   shading_factor = 2,
   direction = "float",
-  shell = vim.o.shell,
+  shell = "nu",
   float_opts = {
     border = "curved",
     highlights = {
