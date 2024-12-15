bea71c3b6e79ecf24afe27c6fd6d1e54216a4002
A. Bryant <W.Sword.09@hotmail.com>
2024-12-15 13:47:44 -0600

    nvim/add flash.nvim

--- Nvim/lua/plugins.lua
+++ Nvim/lua/plugins.lua
@@ -62,7 +62,17 @@ local plugins = {
     cmd = "BufferList",
     opts = {},
   },
+
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    },
+  },
 
   {
     "nvim-neo-tree/neo-tree.nvim",
