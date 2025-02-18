0f98cdf845c05be59c8202d6b67ecd7218697291
A. Bryant <W.Sword.09@hotmail.com>
2025-02-17 22:47:59 -0600

    nvim/remove smear-cursor

--- Nvim/lua/plugins.lua
+++ Nvim/lua/plugins.lua
@@ -28,7 +28,6 @@ local plugins = {
   { "akinsho/bufferline.nvim",             name = "bufferline",       version = "*" },       -- Requires devicons
   { 'nvimdev/dashboard-nvim',              name = "dashboard",        event = "VimEnter", }, -- Requires devicons
   { "lukas-reineke/indent-blankline.nvim", name = "indent-blankline", main = "ibl" },
-  { "sphamba/smear-cursor.nvim",           opts = {} },
 
   {
     "echasnovski/mini.indentscope",
