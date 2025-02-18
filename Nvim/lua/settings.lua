b71dbb6c28bb56997fbd7cb541841e7702f455a0
A. Bryant <W.Sword.09@hotmail.com>
2025-02-17 22:45:13 -0600

    nvim/change titlestring

--- Nvim/lua/settings.lua
+++ Nvim/lua/settings.lua
@@ -27,7 +27,7 @@ o.breakindent    = true
 o.breakindentopt = { "shift:2" }
 o.nrformats      = [[bin,hex,octal,alpha]]
 o.title          = true
-o.titlestring    = [[NVIM - %{expand('%:p:h:t')}%{expand('%:t') == '' ? '' : '/'.expand('%:t')} ]]
+o.titlestring    = [[NVIM - [ %{expand('%:p:h:t')}%{expand('%:t') == '' ? '' : '/'.expand('%:t')} ] ]]
 
 o.guicursor      =
 "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
