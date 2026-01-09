09a2dfb03610381c942c2d37cc45c8c1f270d648
A. Bryant <W.Sword.09@hotmail.com>
2026-01-09 13:48:43 -0600

    yazi/update to version 26.1.4

--- yazi/config/init.lua
+++ yazi/config/init.lua
@@ -1,3 +1,4 @@
 require("full-border"):setup()
 require("starship"):setup()
+require("git"):setup()
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
